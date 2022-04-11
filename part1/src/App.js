24136e5e0c94e62b342549b8d2e3d50e66b4daeb
Catalina Sibisanu <sibisanu.catalinaioana@gmail.com>
2022-04-11 14:42:26 +0300

    Ex 1.9 done (1.8 done before)

--- part1/src/App.js
+++ part1/src/App.js
@@ -8,44 +8,54 @@ const Greeting = () => {
   )
 }
 
+const Statistics = ({ good, bad, neutral, allReviews, avg, pos }) => {
+  if (allReviews === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+  return (
+    <div>
+      <p>good  {good}</p>
+      <p>bad  {bad}</p>
+      <p>neutral  {neutral}</p>
+      <div>all  {allReviews}</div>
+      <div>avg  {avg}</div>
+      <div>pos  {pos} %</div>
+    </div>
+  )
+}
+
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allReviews, setAll] = useState(0)
-  const [avg, setAvg] = useState(0)
-  const [pos, setPos] = useState(0)
-
   const handleGoodReviews = () => {
     setAll(allReviews + 1)
     setGood(good + 1)
-    // setAvg(avg + (1 / allReviews))
     calculateAVG(good + 1, bad, allReviews + 1)
     calculatePositive(good + 1, allReviews + 1)
-  }
-  const handleBadReviews = () => {
-    setAll(allReviews + 1)
-    setBad(bad + 1)
-    // setAvg((avg) / (allReviews + 1))
-    calculateAVG(good, bad + 1, allReviews + 1)
-    calculatePositive(good, allReviews + 1)
-
   }
   const handleNeutralReviews = () => {
     setAll(allReviews + 1)
     setNeutral(neutral + 1)
-    // setAvg((avg) / (allReviews + 1))
     calculateAVG(good, bad, allReviews + 1)
+    calculatePositive(good, allReviews + 1)
+  }
+  const handleBadReviews = () => {
+    setAll(allReviews + 1)
+    setBad(bad + 1)
+    calculateAVG(good, bad + 1, allReviews + 1)
     calculatePositive(good, allReviews + 1)
 
   }
+  const [avg, setAvg] = useState(0)
+  const [pos, setPos] = useState(0)
+  const [allReviews, setAll] = useState(0)
 
   function calculateAVG(good, bad, allReviews) {
-    // console.log(good)
-    // console.log(bad)
-    // console.log(allReviews)
     setAvg((good * 1 + bad * -1) / allReviews)
-
   }
 
   function calculatePositive(good, allReviews) {
@@ -70,13 +80,7 @@ const App = () => {
       <h1>
         statistics
       </h1>
-
-      <p>good  {good}</p>
-      <p>bad  {bad}</p>
-      <p>neutral  {neutral}</p>
-      <p>all  {allReviews} </p>
-      <p>average  {avg} </p>
-      <p>positive   {pos} %</p>
+      <Statistics avg={avg} allReviews={allReviews} pos={pos} good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
