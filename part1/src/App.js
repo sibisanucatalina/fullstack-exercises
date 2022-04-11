821b6b36a19c163a4ee286eb5d5cb1425dd05900
Catalina Sibisanu <sibisanu.catalinaioana@gmail.com>
2022-04-11 16:00:33 +0300

    Ex 1.11 really done

--- part1/src/App.js
+++ part1/src/App.js
@@ -12,10 +12,12 @@ const StatisticLine = ({ text, value }) => {
   return (
     <div>
       <table>
+        <tbody>
         <tr>
           <td>{text}</td>
           <td>{value}</td>
         </tr>
+        </tbody>
       </table>
     </div>
   )
